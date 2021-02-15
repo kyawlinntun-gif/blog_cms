cafa96160968c3beab17848b917e1c4c9c2de988
kyawlinntun <kyawlinntun100@gmail.com>
2021-02-15 10:35:06 +0630

    07 Create Method for Post, 08 Create New Post Form, 09 Create New Post Form 2, 10 Foreach Looping to Display Category Name, 11 Validation for Post Store Method, 12 User Input Data to Database Post Table, 13 Image Upload to Database Post Table, 14 Success Message for Post Creating, and 15 Create New Post Part 1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
     public function index()
     {
         return view('post.main');
+    }
+
+    public function create()
+    {
+        $categories = [];
+
+        foreach(Category::all() as $category)
+        {
+            $categories[$category->id] = $category->name;
+        }
+
+
+        return view('post.create', [
+            'categories' => $categories
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'category_id' => 'required|integer|exists:categories,id',
+            'title' => 'required|min:3|max:20',
+            'author' => 'required|min:3|max:20',
+            'image' => 'required|mimes:jpg,png,jfif',
+            'short_desc' => 'required|min:20|max:50',
+            'description' => 'required|min:50|max:1000'
+        ]);
+
+        if($validator->fails())
+        {
+            return redirect('/posts/create')->withInput()->withErrors($validator);
+        }
+
+        // Get filename with extension
+        $filenameWithExt = $request->file('image')->getClientOriginalName();
+
+        // Get just the filename
+        $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+
+        // Get extension
+        $extension = $request->file('image')->getClientOriginalExtension();
+
+        // Create new filename
+        $filenameToStore = $filename . '_' . time() . '.' .$extension;
+
+        // Create Path
+        $destinationPath = public_path("/img/posts/");
+
+        // Upload image
+        $path = $request->file('image')->move($destinationPath, $filenameToStore);
+
+        Session::flash('success_message', 'Post was created successfully!');
+
+        Post::create(['category_id' => $request->category_id, 'title' => $request->title, 'author' => $request->author, 'image' => $filenameToStore, 'short_desc' => $request->short_desc, 'description' => $request->description]);
+
+        return redirect('/posts/create');
     }
 }
